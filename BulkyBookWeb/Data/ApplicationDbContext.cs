09a4b057d407e68222e416495ae92d2e6cf39e23
Suyog-Kulkarni <98457685+Suyog-Kulkarni@users.noreply.github.com>
2022-12-07 01:58:03 +0530

    Add project files.

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+namespace BulkyBookWeb.Data;
+ public class ApplicationDbContext : DbContext
+ {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+    {
+
+    }
+    // DbContext generally represents a database connection and a set of tables.
+    // DbSet is used to represent a table.
+
+    public DbSet<Category> categories { get; set; } 
+
+}
+
---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key] // tells the database that Id is the primary key 
+        public int Id { get; set; }
+
+        [Required] // tells the database that Name is required
+        public string? Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+        // datetime.now is a static method that returns the current date and time
+
+    }
+}
