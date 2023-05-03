38c582e3a32e195fc3fe252bdbac551767d5428a
Suyog-Kulkarni <98457685+Suyog-Kulkarni@users.noreply.github.com>
2023-05-03 20:10:34 +0530

    Done with small project

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -27,6 +27,7 @@ namespace BulkyBookWeb.Controllers
             return View(objCategoryList);
           
         }
+        
         //GET
         /*Get method is mainly used at the client (Browser) side to send a request to a specified
         server to get certain data or resources.*/
@@ -35,12 +36,7 @@ namespace BulkyBookWeb.Controllers
             return View();
 
         }
-        public IActionResult Sample()
-        {
-           
-            return View ();
-
-        }
+        // ONE CREATE TO SHOW TO VIEW AND ANOTHER TO SEND DATA TO DATABASE 
         
         // POST
         /* Post method is mainly used at the client(Browser) side to send data to a Specified 
@@ -48,8 +44,7 @@ namespace BulkyBookWeb.Controllers
         [HttpPost] 
         [ValidateAntiForgeryToken]
 
-        public IActionResult Create(Category obj) // FIND HOW THIS IS CONNECTED TO CREATE BUTTON 
-            // because it is a post method and we also defined post inside the view
+        public IActionResult Create(Category obj)
         {
             // this method didnt run after pressing createnewcat. because we defined it as post(send data method)
             if (obj.Name == obj.DisplayOrder.ToString())
@@ -58,35 +53,35 @@ namespace BulkyBookWeb.Controllers
                 // will be displayed only under name else use Customerror to display overall using 
                 // all in asp-validation-summary helper tag in view
             }
-           if (ModelState.IsValid)// modelstate.isvalid checks that is it possible to bind the model with the view
-           {
+            if (ModelState.IsValid)// modelstate.isvalid checks that is it possible to bind the model with the view
+            {
 
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-            return Redirect("Index");
-
-           /*return Redirect("Index");*/
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["Success"] = "Category created successfully";
+                return Redirect("Index");
             }
-            
-            return View(obj);
-            
-            
+
+            return View(obj);// to stay on same page returning the same object or else return only view
+
+
 
         }
-        public IActionResult Edit(int id)
+        [HttpGet]
+        public IActionResult Edit(int? id)
         {
-            /*if (id==null || id == 0)
+            if (id == null || id == 0)
             {
                 return NotFound();
-            }*/
+            }
             var categoryfromDb = _db.Categories.Find(id);// retrives the data from database by using id
-
-            /*var category = _db.Categories.FirstOrDefault(u => u.Id == id);
+/*
+            var category = _db.Categories.FirstOrDefault(u => u.Id == id);
             var categorys = _db.Categories.SingleOrDefault(u => u.Id == id);*/
-            /*if(categoryfromDb is null)
+            if (categoryfromDb is null)
             {
                 return NotFound();
-            }*/
+            }
             return View(categoryfromDb);
 
         }
@@ -102,28 +97,56 @@ namespace BulkyBookWeb.Controllers
          // because it is a post method and we also defined post inside the view
         {
             // this method didnt run after pressing createnewcat. because we defined it as post(send data method)
-            if (obj.Name == obj.DisplayOrder.ToString())
+            /*if (obj.Name == obj.DisplayOrder.ToString())
             {
                 ModelState.AddModelError("Name", "Name and Display Order cannot be same.");
                 // will be displayed only under name else use Customerror to display overall using 
                 // all in asp-validation-summary helper tag in view
-            }
-            if (ModelState.IsValid)// modelstate.isvalid checks that is it possible to bind the model with the view
+            }*/
+            if (ModelState.IsValid)
             {
-
-                _db.Categories.Add(obj);
+                _db.Categories.Update(obj);
                 _db.SaveChanges();
-                return Redirect("Index");
-
-                /*return Redirect("Index");*/
+                TempData["Success"] = "Category updated successfully";
+                return RedirectToAction("Index");
             }
-
-            return View(obj);
-
-
+               return View(obj);// to stay on same page returning the same object or else return only view
+        }
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryfromDb = _db.Categories.Find(id);// retrives the data from database by using id
+            /*
+                        var category = _db.Categories.FirstOrDefault(u => u.Id == id);
+                        var categorys = _db.Categories.SingleOrDefault(u => u.Id == id);*/
+            if (categoryfromDb is null)
+            {
+                return NotFound();
+            }
+            return View(categoryfromDb);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            var item = _db.Categories.Find(id);
+            if(item is null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(item);
+            _db.SaveChanges();
+            TempData["Success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+            /*
+             by getting obj use
+            _db.cat.remove(obj);
+            */
 
         }
-
 
     }
 }
--- BulkyBookWeb/Models/Info.cs
+++ BulkyBookWeb/Models/Info.cs
@@ -1,32 +1,17 @@
-﻿
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookWeb.Models
 {
-    public class Category
+    public class Info
     {
         [Key] // tells the database that Id is the primary key 
         public int Id { get; set; }
 
         [Required] // tells the database that Name is required
-        [DisplayName("Category Name")]
-        [MaxLength(30)]
         public string? Name { get; set; }
-
-        [DisplayName("Display Order")]
-        [Range(0,100,ErrorMessage="Display Order must be between 1 and 100 only!!")]
         public int DisplayOrder { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
         // datetime.now is a static method that returns the current date and time
-
-      
-        public static Category fun()
-        {
-            Category c = new();
-            return c;
-        }
-
-    } 
+    }
 }
