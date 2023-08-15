561e6fb183626140d3d952f8295d6075535b1d83
Suyog-Kulkarni <98457685+Suyog-Kulkarni@users.noreply.github.com>
2023-08-16 01:08:46 +0530

    Added a dropdown for company

---
+++ Bulky.DataAccess/Migrations/20230815181240_addCompanyToUser.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBookWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCompanyToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CompanyId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 8, 15, 23, 42, 39, 792, DateTimeKind.Local).AddTicks(347));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 8, 15, 23, 42, 39, 792, DateTimeKind.Local).AddTicks(359));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 8, 15, 23, 42, 39, 792, DateTimeKind.Local).AddTicks(361));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_CompanyId",
+                table: "AspNetUsers",
+                column: "CompanyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Companies_CompanyId",
+                table: "AspNetUsers",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Companies_CompanyId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_CompanyId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyId",
+                table: "AspNetUsers");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 8, 15, 16, 57, 3, 400, DateTimeKind.Local).AddTicks(9012));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 8, 15, 16, 57, 3, 400, DateTimeKind.Local).AddTicks(9022));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 8, 15, 16, 57, 3, 400, DateTimeKind.Local).AddTicks(9023));
+        }
+    }
+}
--- Bulky.Models/ApplicationUser.cs
+++ Bulky.Models/ApplicationUser.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +20,11 @@ public class ApplicationUser : IdentityUser
     public string? State { get; set; }
     public string? PostalCode { get; set; }
 
+    public int? CompanyId { get; set; }
+    [ForeignKey("CompanyId")]
+    [ValidateNever]// this is used to prevent the company object from being validated because it is not being used in the view 
+    public Company Company { get; set; }// this not going to add in the database but it is going to be used to get the company object from the database
+    // this is a just a navigation property to the Company table so it will not be added to the database 
 
 }
 
